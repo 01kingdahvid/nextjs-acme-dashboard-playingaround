66455f37b11e1a145643eb2b1dc3c818f5703e82
David Onyekwere <davidchibuzor.ony@gmail.com>
2024-11-11 19:08:32 +0100

    upp

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,4 +1,8 @@
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { 
+  // PencilIcon,
+   PlusIcon, 
+  // TrashIcon
+ } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 export function CreateInvoice() {
@@ -13,24 +17,24 @@ export function CreateInvoice() {
   );
 }
 
-export function UpdateInvoice({ id }: { id: string }) {
-  return (
-    <Link
-      href="/dashboard/invoices"
-      className="rounded-md border p-2 hover:bg-gray-100"
-    >
-      <PencilIcon className="w-5" />
-    </Link>
-  );
-}
+// export function UpdateInvoice({ id }: { id: string }) {
+//   return (
+//     <Link
+//       href="/dashboard/invoices"
+//       className="rounded-md border p-2 hover:bg-gray-100"
+//     >
+//       <PencilIcon className="w-5" />
+//     </Link>
+//   );
+// }
 
-export function DeleteInvoice({ id }: { id: string }) {
-  return (
-    <>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
-      </button>
-    </>
-  );
-}
+// export function DeleteInvoice({ id }: { id: string }) {
+//   return (
+//     <>
+//       <button className="rounded-md border p-2 hover:bg-gray-100">
+//         <span className="sr-only">Delete</span>
+//         <TrashIcon className="w-5" />
+//       </button>
+//     </>
+//   );
+// }
