2ad45dc183fa822e687f7b1268a439491176c61c
David Onyekwere <davidchibuzor.ony@gmail.com>
2024-11-15 17:22:37 +0100

    upp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -8,13 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/opengraph-image.png"
-    />
-      <title>Acme Dc</title>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/opengraph-image.png"
+      />
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
